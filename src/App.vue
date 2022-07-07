e6f4bea297980936eb5d7ffd0a9d30e41b12c462
sosokim9966 <sosokim9966@gmail.com>
2022-07-07 09:54:39 +0900

    2022/07/07-app2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,78 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!--애플리케이션 이름 표시-->
+    <todo-header></todo-header>
+    <!--할일 입력 및 추가-->
+    <todo-input v-on:addTodo="addTodo"></todo-input>
+    <!--할 일 목록 표시 및 특정 할 일 삭제-->
+    <todo-list v-bind:propsdata="todoItems" @removeTodo="removeTodo"></todo-list>
+    <!--할 일 모두 삭제-->
+    <todo-footer v-on:removeAll="clearAll"></todo-footer>
   </div>
+
 </template>
 
 <script>
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todoItems: []
     }
+  },
+  methods: {
+    // 인자값 todoItem = TodoInput 컴포넌트에서 올려 보낸 할 일 텍스트 값
+    addTodo(todoItem) {
+      // TodoInput 컴포넌트에서 App 컴포넌트로 신호(이벤트)를 보내 App 컴포넌트의 addTodo() 메서드를 실행
+      localStorage.setItem(todoItem, todoItem);
+      // App 컴포넌트의 todoItems 데이터 속성에 추가
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    }
+
+  },
+  // 뷰의 인스턴스가 생성되자마자 뷰 데이터에 접근 할 수 있도록 created() 라이프 사이클 훅에서 로컬 스토리지의 데이터를 뷰 데이터로 옮김.
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  body{
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input{
+    border-style: groove;
+    width: 200px;
+  }
+  button{
+    border-style: groove;
+  }
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  methods: {
+    removeTodo(todoItem, index) {
+      // removeItem() -> todoItem 인자를 사용하여 로컬 스토리지에서 할 일 텍스트 삭제
+      //localStorage.removeItem(todoItem);
+      // splice() -> 인자로 받은 index를 이용하여 배열의 해당 인덱스에서 1만큼 삭제
+      //this.todoItems.splice(index,1);
+      this.$emit('removeTodo', todoItem, index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
